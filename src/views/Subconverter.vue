8d54d636aa015f5d5b7536d0c7c1882e9649b246
tokuwaka <82072644+tokuwakana@users.noreply.github.com>
2022-08-12 11:16:56 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -261,11 +261,71 @@ export default {
             label: "universal",
             options: [
               {
-                label: "rule",
+                label: "No-Urltest",
                 value:
-                  "https://raw.githubusercontent.com/tokuwakana/rules/main/ClashRule.ini"
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/universal/no-urltest.ini"
               },
               {
+                label: "Urltest",
+                value:
+                  "https://raw.githubusercontent.com/tokuwakana/rules/main/ClashRule.ini"
+              }
+            ]
+          },
+          {
+            label: "customized",
+            options: [
+              {
+                label: "Maying",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/maying.ini"
+              },
+              {
+                label: "Ytoo",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/ytoo.ini"
+              },
+              {
+                label: "FlowerCloud",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/flowercloud.ini"
+              },
+              {
+                label: "Nexitally",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/nexitally.ini"
+              },
+              {
+                label: "SoCloud",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/socloud.ini"
+              },
+              {
+                label: "ARK",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/ark.ini"
+              },
+              {
+                label: "ssrCloud",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/customized/ssrcloud.ini"
+              }
+            ]
+          },
+          {
+            label: "Special",
+            options: [
+              {
+                label: "NeteaseUnblock(仅规则，No-Urltest)",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/special/netease.ini"
+              },
+              {
+                label: "Basic(仅GEOIP CN + Final)",
+                value:
+                  "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/special/basic.ini"
+              }
+            ]
           }
         ]
       },
